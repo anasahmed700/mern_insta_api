dd4eb1baa39e078acd862ee6968ad6b744812b01
Anas Ahmed <anasahmed700@gmail.com>
2022-07-01 00:04:32 +0500

    posting user object to db

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,44 @@
 /* jshint esversion: 6 */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
-router.get('/', (req, res) => {
-  console.log('sending get req to home');
+router.get("/", (req, res) => {
+  console.log("sending get req to home");
   res.send("Hello");
 });
 
-router.post('/signup', (req, res) => {
-  const {name, email, password} = req.body;
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    return res.status(422).json({error: "Please add all required fields!"});
+    return res.status(422).json({ error: "Please add all required fields!" });
   }
-  res.json({message: "Record has been added"});
+
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({ error: "User already exist!" });
+      }
+
+      const user = new User({
+        email,
+        name,
+        password,
+      });
+
+      user.save()
+        .then((user) => {
+          res.json({ message: "User has been saved", data:user });
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
